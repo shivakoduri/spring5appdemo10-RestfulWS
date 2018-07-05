b229bf1c19e6639c3bfe4fd7d521ba45b3793652
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-07-05 22:03:05 +0530

    Initial commit for Restful webservices

---
+++ src/main/java/com/myprojects/spring/examples/spring5RestWS/Spring5RestWsApplication.java
@@ -0,0 +1,12 @@
+package com.myprojects.spring.examples.spring5RestWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RestWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RestWsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/myprojects/spring/examples/spring5RestWS/Spring5RestWsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myprojects.spring.examples.spring5RestWS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5RestWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
