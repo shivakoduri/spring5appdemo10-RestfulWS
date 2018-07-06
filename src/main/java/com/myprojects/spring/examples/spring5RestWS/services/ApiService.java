e85942a8ccd36fb80a0eddb890cda9c83ae71763
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-07-06 20:42:05 +0530

    Initial commit for Reactive with Spring WebClient

--- src/main/java/com/myprojects/spring/examples/spring5RestWS/services/ApiService.java
+++ src/main/java/com/myprojects/spring/examples/spring5RestWS/services/ApiService.java
@@ -1,10 +1,14 @@
 package com.myprojects.spring.examples.spring5RestWS.services;
 
 import com.myprojects.spring.examples.api.domain.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface ApiService {
 
     List<User> getUsers(Integer limit);
+
+    Flux<User>  getUsers(Mono<Integer> limit);
 }
